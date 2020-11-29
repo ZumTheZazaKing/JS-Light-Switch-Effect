c7947c301d96c967cd9b92a5ee6df0458ff9e82a
Purogurama Z <73376015+ZumTheZazaKing@users.noreply.github.com>
2020-11-29 13:01:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function turnOn(){
+
+    document.getElementById('off').classList.add('hide');
+
+    document.getElementById('on').classList.remove('hide');
+
+}
+
+function turnOff(){
+
+    document.getElementById('on').classList.add('hide');
+
+    document.getElementById('off').classList.remove('hide');
+
+}
